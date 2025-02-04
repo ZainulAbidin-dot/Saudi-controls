55a9a6fbfa0703442b3d1109ceda586cd68622ee
siddiq <siddiqahmed882@gmail.com>
2025-02-04 13:40:32 +0500

    fix css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
-import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  site: "https://vbartalis.github.io",
-  base: "/agency-template",
+  site: 'https://saudicontrols.vercel.app/',
 });
